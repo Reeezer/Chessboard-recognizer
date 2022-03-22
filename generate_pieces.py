c3855910fc374bf302028e305f24abea325502e8
Léon Muller <leonmuller@hotmail.fr>
2022-03-22 18:17:32 +0100

    Generate pieces and background combination, and test template matching

---
+++ generate_pieces.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+from PIL import Image
+
+def get_images(pieces_path, backgrounds_path):
+    pieces_paths = [piece_path for piece_path in os.listdir(pieces_path)]
+    pieces_img = [Image.open(os.path.join(pieces_path, piece_path)) for piece_path in pieces_paths]
+    
+    backgrounds_paths = [background_path for background_path in os.listdir(backgrounds_path)]
+    backgrounds_img = [Image.open(os.path.join(backgrounds_path, background_path)) for background_path in backgrounds_paths]
+    
+    return pieces_img, pieces_paths, backgrounds_img, backgrounds_paths
+
+def create_pieces(pieces_img, pieces_path, backgrounds_img, backgrounds_path):
+    created_pieces_img = []
+    created_pieces_path = []
+    
+    for bg_i in range(len(backgrounds_img)):
+        for piece_i in range(len(pieces_img)):
+            new_path = f"{pieces_path[piece_i][:-4]}_{backgrounds_path[bg_i][:-4]}.png"
+            
+            background = backgrounds_img[bg_i]
+            piece = pieces_img[piece_i]
+            
+            new_image = background.copy()
+            new_image.paste(piece, (0,0), mask=piece.convert('RGBA'))
+            
+            created_pieces_img.append(new_image)
+            created_pieces_path.append(new_path)
+            
+    return created_pieces_img, created_pieces_path
+
+def save_pieces(pieces_img, pieces_path, backgrounds_img, backgrounds_path, path):
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+
+    [pieces_img[i].save(os.path.join(path, pieces_path[i])) for i in range(len(pieces_img))]
+    [backgrounds_img[i].save(os.path.join(path, backgrounds_path[i])) for i in range(len(backgrounds_img))]
+
+if __name__ == '__main__':
+    pieces_path = 'pieces_screens/pieces/'
+    backgrounds_path = 'pieces_screens/backgrounds/'
+    path = 'pieces/'
+    
+    pieces_img, pieces_paths, backgrounds_img, backgrounds_paths = get_images(pieces_path, backgrounds_path)
+    created_pieces_img, created_pieces_path = create_pieces(pieces_img, pieces_paths, backgrounds_img, backgrounds_paths)
+    save_pieces(created_pieces_img, created_pieces_path, backgrounds_img, backgrounds_paths, path)
--- script.py
+++ script.py
@@ -3,54 +3,39 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import os
 
+from board_finder import getBoardCoords, imageResize
+
 # https://python-chess.readthedocs.io/en/latest/
 # To output again the board
 
 methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
 meth = 'cv.TM_CCORR_NORMED'
-pieces_names = ['bishop', 'pawn', 'king', 'queen', 'knight', 'rock']
 
 def get_pieces_images(path):
-    black_pieces_path = [f"{path}black_{name}.png" for name in pieces_names]
-    white_pieces_path = [f"{path}white_{name}.png" for name in pieces_names]
-    pieces_path = [black_pieces_path, white_pieces_path]
-
-    black_pieces_img = [cv.imread(path) for path in black_pieces_path]
-    white_pieces_img = [cv.imread(path) for path in white_pieces_path]
-    pieces_img = [black_pieces_img, white_pieces_img]
- 
+    pieces_path = [piece_path for piece_path in os.listdir(path)]
+    pieces_img = [cv.imread(os.path.join(path, piece_path)) for piece_path in pieces_path]
     return pieces_path, pieces_img
 
-def get_boards_images(path):
-    board_cells_path = [f"{path}{x}" for x in os.listdir(path)]
-    board_cells_img = [cv.imread(path) for path in board_cells_path]
-    
-    return board_cells_path, board_cells_img
-
-def display_pieces(pieces, path):
+def display_pieces(pieces, paths):
     print("Pieces:")
-    for color in range(2):
-        for img in range(6):
-            print(path[color][img])
-            cv.imshow(path[color][img], pieces[color][img])
+    for i in range(len(pieces)):
+        print(paths[i])
+        cv.imshow(paths[i], pieces[i])
     cv.waitKey(0) 
     cv.destroyAllWindows()
     
 def recognize_piece(paths, pieces, cell):  
     max_value = float('-inf')
-    max_method = ''
     max_image_path = ''
     max_image = None
     
-    for color in range(2):
-        for img in range(6):
-            piece = pieces[color][img]
-            # piece = cv.cvtColor(piece, cv.COLOR_BGR2GRAY)
-            path = paths[color][img]
-            w, h = piece.shape[:-1]
-            
-            # cell_copy = cv.cvtColor(cell, cv.COLOR_BGR2GRAY)
+    for i in range(len(pieces)):
+        piece = pieces[i]
+        path = paths[i]
+        
+        for scale in range(20):
             cell_copy = cell.copy()
+            cell_copy = imageResize(cell_copy, 0.1*(scale+1))
             
             # Apply template Matching
             method = eval(meth)
@@ -59,45 +44,31 @@ def recognize_piece(paths, pieces, cell):
             
             # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
             if method in [cv.TM_SQDIFF]:
-                top_left = min_loc
                 max = min_val
             else:
-                top_left = max_loc
                 max = max_val
                 
             if max > max_value:
                 max_value = max
-                max_method = meth
                 max_image = piece
                 max_image_path = path
                 
-            # Display the result
-            # bottom_right = (top_left[0] + w, top_left[1] + h)
-            # cv.rectangle(img, top_left, bottom_right, 255, 2)
-            # plt.subplot(221), plt.imshow(res, cmap='gray')
-            # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-            # plt.subplot(222), plt.imshow(img, cmap='gray')
-            # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-            # plt.subplot(223), plt.imshow(piece, cmap='gray')
-            # plt.title('Piece'), plt.xticks([]), plt.yticks([])
-            # plt.suptitle(f"{meth} - {path}")
-            # plt.show()
-                
     print()
     print(max_image_path)
-    print(max_value)
-    print(max_method)
-    cv.imshow("board's cell", cell)
+    print(f"Best: {max_value:.3f}")
+    cv.imshow("Board's cell", cell)
     cv.imshow(max_image_path, max_image)
     cv.waitKey(0) 
     cv.destroyAllWindows()
 
 if __name__ == '__main__':
     piece_path = 'pieces/'
-    board_path = 'board/'
+    board_path = 'Board_Examples/medium2.png'
+    
+    board_img = imageResize(cv.imread(board_path), 0.5)
+    board_cells_img = getBoardCoords(board_img)
     
     pieces_path, pieces_img = get_pieces_images(piece_path)
-    board_cells_path, board_cells_img = get_boards_images(board_path)
     
     # display_pieces(pieces_img, pieces_path)
     
